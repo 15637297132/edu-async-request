7c8872e8284cdf196a3a4858292c7d80fb2ef3f0
yangzhen <p7_1994@163.com>
2020-03-07 23:28:51 +0800

    优化代码

---
+++ src/main/java/com/p7/framework/async/request/base/BaseRequestProcessor.java
@@ -0,0 +1,12 @@
+package com.p7.framework.async.request.base;
+
+/**
+ * 处理请求线程
+ *
+ * @author yz
+ * @ClassName: BaseRequestProcessor
+ * @date 2018年11月8日 下午6:38:47
+ */
+public interface BaseRequestProcessor extends Runnable {
+
+}
